597657287416d8e0adb6bb8eeb07ca79ebef119c
Kamil Grajski <kgrajski@nurosci.com>
2025-02-20 17:47:58 +0000

    Basic Spatial and Time Series Visualizations From Raw Data Done

--- SpeechBCI.py
+++ SpeechBCI.py
@@ -1,61 +1,134 @@
-
-import itertools
-import math
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 import numpy as np
-import os
 import pandas as pd
-import random
-import time
+import plotly.express as px
+import plotly.io as pio
 
-class SpkPowArray:
-        #
-        # The most basic SpkPowerArray object is an 8x8 electrode array.
-        # SpeechBCI packages the data in a 3D numpy array.
-        #
-
-    def __init__(self, session_id, block_id, trial_id, spkpowarray, start_chan, end_chan, array_dim=8):
-        self.session_id = session_id # Typically a date
+class ElectrodeArray:
+    #
+    # A basic electrode array class: time series of 2D array of channels.
+    # It is the result obtained after doing data extraction from some
+    # given data source (e.g., Willett, et al. (2023) data).
+    # Goal: organize things so that they play nicely with PyTorch.
+    #
+    # The results for self.xt is that one can reference the data by 2D
+    # array coordinates where (0,0) is the anterior, superior electrode.
+    # Rows represent superiot to inferior.
+    # Columns represent anterior to posterior.
+    #
+    def __init__(self, description, session_id, block_id, trial_id, xt,
+                 start_chan, end_chan, num_rows=16, num_cols=8):
+        self.session_id = session_id  # Typically a date or file name.
+        self.desc = description # convention: use raw data variable name
         self.block_id = block_id
         self.trial_id = trial_id
         self.idkey = f'{session_id}_{block_id}_{trial_id}'
-        self.num_samples = spkpowarray.shape[0]
-        self.spkpowarray = self.extract_area6v(spkpowarray, start_chan, end_chan, array_dim)
-        
-    def extract_area6v(self, spkpowarray, start_chan, end_chan, array_dim):
-        return np.reshape(spkpowarray[:,start_chan:end_chan], (spkpowarray.shape[0], array_dim, array_dim))
-        
+        self.num_samples = xt.shape[0]
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.xt = self.extract_area6v(xt, start_chan, end_chan)
+
+    def extract_area6v(self, xt, start_chan, end_chan):
+        # In Willits, et al. (2023) Competition Data there is an
+        # odd mapping of channel number to electrode position.
+        # We want to store the data in a way that reflects the
+        # physical layout of the electrodes. Note that the channel
+        # numbers are 0-indexed.
+        chan_to_electrode_map = [62, 51, 43, 35, 94, 87, 79, 78,
+                                    60, 53, 41, 33, 95, 86, 77, 76,
+                                    63, 54, 47, 44, 93, 84, 75, 74,
+                                    58, 55, 48, 40, 92, 85, 73, 72,
+                                    59, 45, 46, 38, 91, 82, 71, 70,
+                                    61, 49, 42, 36, 90, 83, 69, 68,
+                                    56, 52, 39, 34, 89, 81, 67, 66,
+                                    57, 50, 37, 32, 88, 80, 65, 64,
+                                    125, 126, 112, 103, 31, 28, 11, 8,
+                                    123, 124, 110, 102, 29, 26, 9, 5,
+                                    121, 122, 109, 101, 27, 19, 18, 4,
+                                    119, 120, 108, 100, 25, 15, 12, 6,
+                                    117, 118, 107, 99, 23, 13, 10, 3,
+                                    115, 116, 106, 97, 21, 20, 7, 2,
+                                    113, 114, 105, 98, 17, 24, 14, 0,
+                                    127, 111, 104, 96, 30, 22, 16, 1]
+        extracted_xt = np.zeros((self.num_samples, len(chan_to_electrode_map)))
+        for iel in range(len(chan_to_electrode_map)):
+            extracted_xt[:, iel] = xt[:, chan_to_electrode_map[iel]]
+            
+        return extracted_xt
     
-    def implot(self, time_start, time_end, time_step=1, interval=200):
+        #
+        # Using plotly express, plot the electrode array (xt) as a
+        # time-series of 2D image heatmaps.
+        # Note: the data stored as 1D array, so reshape to 2D.
+        #
+    def implot(self, time_start, time_end, time_step=1, interval=100):
+        pio.renderers.default = "browser"
         
-            # Create a figure and axes
-        fig, ax = plt.subplots()
-
-            # Initialize the plot
-        im = ax.imshow(self.spkpowarray[0], cmap='hot', origin='upper')
-        cbar = plt.colorbar(im)
-        ax.set_title(self.idkey)
-
-            # Animation update function
-        def update(itx):
-            im.set_data(self.spkpowarray[itx])
-            return [im]
-
-        ani = FuncAnimation(fig,
-                            update,
-                            frames=np.arange(time_start, time_end, time_step),
-                            interval=interval,
-                            repeat=True,
-                            blit=True)
-        plt.show()
+        # Data Integrity Check - reshaping can be a common source of errors
+        num_frames = (time_end - time_start) // time_step
+        expected_size = num_frames * self.num_rows * self.num_cols
+        data = self.xt[time_start:time_end:time_step]
+        if data.size != num_frames * self.num_rows * self.num_cols:
+            raise ValueError(
+                f"Data size mismatch: expected {expected_size}, "
+                f"got {data.size}")
         
-    def tsplot(self, ix, iy):
-            # Create the plot
-        x = range(self.num_samples)
-        y = self.spkpowarray[:,ix,iy]
-        plt.plot(x, y)
-        plt.xlabel('Time')
-        plt.ylabel('Amplitude')
-        plt.title('Time Series For: [' + str(ix) + ', ' + str(iy) + ']')
-        plt.show()
+        data = data.reshape((-1, self.num_rows, self.num_cols))
+        
+        fig = px.imshow(data, animation_frame=0,
+                        color_continuous_scale="Hot", origin='upper',
+                        aspect='equal',
+                        labels=dict(animation_frame="Time Step",
+                                    x="X-axis", y="Y-axis",
+                                    color=self.desc))
+        
+        fig.update_layout(
+            title=f'{self.desc} Array: {self.idkey}',
+            xaxis_title="X-axis",
+            yaxis_title="Y-axis"
+        )
+        
+        # Safely set animation speed
+        if fig.layout.updatemenus and fig.layout.updatemenus[0].buttons:
+            fig.layout.updatemenus[0].buttons[0].args[1]["frame"][
+                "duration"] = interval
+        
+        return fig
+            
+            #
+            # Using plotly express, plot a specified set of electrode array
+            # x,y positions as time series using stack and facet features to
+            # create one plot per electrode.
+            # For each time point, the sptiaal data stored at each as 1D array.
+            # Indicate starting and ending spatial points as linear indices.
+            # Then reshape the data to 2D.
+            # start_row and end_row - use Python indexing convention.
+            #
+    def tsplot(self, start_row, end_row, interval=100, addl_text=''):
+        pio.renderers.default = "browser"
+    
+        # Data Integrity Check
+        expected_rows = end_row - start_row
+        if not (0 < expected_rows <= self.num_rows):
+            raise ValueError(
+                f"Invalid row range: expected 1 to {self.num_rows}, "
+                f"got {expected_rows}")
+    
+        istart = start_row * self.num_cols
+        iend = end_row * self.num_cols
+        df = pd.DataFrame(self.xt[:,istart:iend].reshape(
+            (self.num_samples, expected_rows * self.num_cols)))
+
+        df = df.stack().reset_index()
+        df.columns = ['time', 'el', 'val']
+    
+        fig = px.line(df, x='time', y='val', facet_col='el',
+                      facet_col_wrap=self.num_cols,
+                      title=f'{self.desc} {addl_text} Time Series:\n {self.idkey}')
+        fig.update_layout(xaxis_title="Time", yaxis_title=self.desc)
+    
+        # Safe Animation Duration Update
+        if fig.layout.updatemenus and fig.layout.updatemenus[0].buttons:
+            fig.layout.updatemenus[0].buttons[0].args[1]["frame"][
+                "duration"] = interval
+    
+        return fig
--- firstpeek.py
+++ firstpeek.py
@@ -12,7 +12,7 @@ import scipy
 from scipy.stats import multivariate_normal
 import time
 
-from SpeechBCI import SpkPowArray
+from SpeechBCI import ElectrodeArray
 
 mat_file_path = './data/competitionData/train/t12.2022.05.05.mat'
 
@@ -30,18 +30,48 @@ except FileNotFoundError:
     print(f"Error: The file '{mat_file_path}' was not found.")
 except Exception as e:
     print(f"An error occurred: {e}")
-    
+
+
 # Create a SpkPowArray object
 
 session_id = 't12.2022.05.05'
-block_id = int(mat_data['blockIdx'][0])
+block_id = int(mat_data['blockIdx'][0][0])
 trial_id = 0
 start_chan = 0
-end_chan = 64
-print("IN: ",session_id, block_id, trial_id, mat_data['spikePow'][0,trial_id].shape, type(mat_data['spikePow'][0,trial_id]))
-spkpow = SpkPowArray('t12.2022.05.05', block_id, trial_id, mat_data['spikePow'][0,trial_id], start_chan, end_chan)
-print(spkpow.idkey, spkpow.num_samples, spkpow.spkpowarray.shape)
-print(spkpow.spkpowarray)
-spkpow.implot(0, spkpow.num_samples, 1)
+end_chan = 128
 
-spkpow.tsplot(5,5)
+if 1:
+    desc = 'spikePow'
+    spkpow = ElectrodeArray(desc, session_id, block_id, trial_id, mat_data[desc][0,trial_id], start_chan, end_chan)
+    
+    fig = spkpow.implot(0, spkpow.num_samples, 1)
+    fig.write_html(f'./figs/competitionData/train/{spkpow.idkey}_implot.html', auto_open=False)
+    fig.show()
+
+    addl_text='Superior'
+    fig = spkpow.tsplot(0, 8, addl_text=addl_text)
+    fig.write_html(f'./figs/competitionData/train/{spkpow.idkey}_{addl_text}_tsplot.html', auto_open=False)
+    fig.show()
+    
+    addl_text='Ventral'
+    fig = spkpow.tsplot(8, 16, addl_text=addl_text)
+    fig.write_html(f'./figs/competitionData/train/{spkpow.idkey}_{addl_text}_tsplot.html', auto_open=False)
+    fig.show()
+
+if 1:
+    desc = 'tx1'
+    tx1 = ElectrodeArray(desc, session_id, block_id, trial_id, mat_data[desc][0,trial_id], start_chan, end_chan)
+    
+    fig = tx1.implot(0, tx1.num_samples, 1)
+    fig.write_html(f'./figs/competitionData/train/{tx1.idkey}_implot.html', auto_open=False)
+    fig.show()
+
+    addl_text='Superior'
+    fig = tx1.tsplot(0, 8, addl_text=addl_text)
+    fig.write_html(f'./figs/competitionData/train/{tx1.idkey}_{addl_text}_tsplot.html', auto_open=False)
+    fig.show()
+    
+    addl_text='Ventral'
+    fig = tx1.tsplot(8, 16, addl_text=addl_text)
+    fig.write_html(f'./figs/competitionData/train/{tx1.idkey}_{addl_text}_tsplot.html', auto_open=False)
+    fig.show()
